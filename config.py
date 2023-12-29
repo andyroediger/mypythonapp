11473f86cf1a1378c737380121f962ea2df4a500
andyroediger <73503684+andyroediger@users.noreply.github.com>
2023-12-29 09:56:57 -0700

    pw

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 hostname = 'database-1.czmhawgybkdk.us-east-1.rds.amazonaws.com'
-username = 'awsadmin'
-password = 'awspassword'
+username = 'admin'
+password = '745wmontevista'
 database = 'aws_database'
